f180f3a4e5fa0531c49245b91124650050fa68f1
syclover <syclover@yeah.net>
2021-10-29 15:48:54 +0800

    8-6

---
+++ src/matrix.java
@@ -0,0 +1,12 @@
+public class matrix {
+    public static double[][] multiplyMatrix(double[][] a,double[][] b){
+        double[][] c=new double[2][2];
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[i].length;j++){
+                for(int d=0;d<a[i].length;d++)
+                    c[i][j]+=a[i][d]*b[d][j];
+            }
+        }
+        return c;
+    }
+}
